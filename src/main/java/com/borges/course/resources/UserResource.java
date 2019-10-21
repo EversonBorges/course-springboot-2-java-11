b03f898cc949ca6f3b0f936b16d4582396b68019
Everson Borges <everson.cursos@gmail.com>
2019-10-21 20:19:37 -0300

    User Entity and Resource

---
+++ src/main/java/com/borges/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.borges.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.borges.course.entities.User;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "945129878", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
